02396b9a5a258e9829e7c0702bf28ed753ecd070
Hoeeeeeh <hoeeeeeh@naver.com>
2024-06-04 22:09:54 +0900

    [Gold II] Title: 교환, Time: 80 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Gold/1039. 교환/교환.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+read = sys.stdin.readline
+
+
+def bfs(N, K):
+    M = len(str(N))
+    visited = set()
+    visited.add((N, 0))
+    q = deque()
+    q.append((N, 0))
+    answer = 0
+    while q:
+        n, k = q.popleft()
+        if k == K:
+            answer = max(answer, n)
+            continue
+        n = list(str(n))
+        for i in range(M - 1):
+            for j in range(i + 1, M):
+                if i == 0 and n[j] == '0':
+                    continue
+                n[i], n[j] = n[j], n[i]
+                nn = int(''.join(n))
+                if (nn, k + 1) not in visited:
+                    q.append((nn, k + 1))
+                    visited.add((nn, k + 1))
+                n[i], n[j] = n[j], n[i]
+    return answer if answer else -1
+
+
+N, K = map(int, read().split())
+print(bfs(N, K))
