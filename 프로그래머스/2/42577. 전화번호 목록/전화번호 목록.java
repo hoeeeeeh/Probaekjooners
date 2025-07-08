83c89b091f3adc63b34721b52a3cd8b263d4a01a
Hoeeeeeh <hoeeeeeh@naver.com>
2025-07-08 21:36:36 +0900

    [level 2] Title: 전화번호 목록, Time: 415.82 ms, Memory: 302 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,12 +2,42 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = false;
-        Arrays.sort(phone_book);
-        for(int i = 0; i < phone_book.length - 1; i++) {
-            answer |= phone_book[i+1].startsWith(phone_book[i]);
+        TrieNode trie = new TrieNode();
+
+        for(String phone : phone_book) {
+            TrieNode ctx = trie;
+            for(int i = 0; i < phone.length(); i++) {
+                char c = phone.charAt(i);
+
+                if(ctx.children.containsKey(c)) {
+                    ctx = ctx.children.get(c);
+                } else {
+                    TrieNode next = new TrieNode();
+                    ctx.children.put(c, next);
+                    ctx = next;
+                }
+                
+                if(ctx.isLeaf) {
+                    return false;
+                }
+                
+                if(i != (phone.length() - 1)) {
+                    continue;
+                }
+                
+                ctx.isLeaf = true;
+                
+                if(ctx.children.size() > 0) {
+                    return false;
+                }
+            }
         }
         
-        return !answer;
+        return true;
     }
+}
+
+class TrieNode {
+    HashMap<Character, TrieNode> children = new HashMap<>();
+    boolean isLeaf = false;
 }
