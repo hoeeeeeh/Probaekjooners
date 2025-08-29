7ecd7ec98cea6f2542886e15e1c800a7122663be
Hoeeeeeh <hoeeeeeh@naver.com>
2025-08-29 16:25:54 +0900

    [level 2] Title: 타겟 넘버, Time: 2.23 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,31 +1,13 @@
 function solution(numbers, target) {
-    var answer = 0;
-    const q = [{value : 0, cnt : 0 }];
-    const visited = new Set();
-    
-    while (q.length > 0) {
-        const { value, cnt } = q.pop();
-        
-        if(cnt === numbers.length) {
-            if(value === target) {
-                answer += 1;
-            }
-            continue;
-        }
-        
-        const plus = { value : value + numbers[cnt], cnt : cnt + 1 };
-        const minus = { value : value - numbers[cnt], cnt : cnt + 1 };
-        
-        if(!visited.has(plus)){
-            visited.add(plus);
-            q.push(plus);
-        }
-        
-        if(!visited.has(minus)){
-            visited.add(minus);
-            q.push(minus);
-        }
+  let dp = { 0: 1 };
+  for (const num of numbers) {
+    const next = {};
+    for (const k in dp) {
+      const s = Number(k), c = dp[k];
+      next[s + num] = (next[s + num] || 0) + c;
+      next[s - num] = (next[s - num] || 0) + c;
     }
-    return answer;
+    dp = next;
+  }
+  return dp[target] || 0;
 }
-
