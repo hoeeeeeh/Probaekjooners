0ca42c0ad023614bda16e1bb4213a5f4c274a8bf
Hoeeeeeh <hoeeeeeh@naver.com>
2025-09-02 15:13:10 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 45.20 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,28 +1,13 @@
 function solution(numbers) {
-    let answer = Array(numbers.length).fill(-1);
-    let biggerNumberIndex = Array(numbers.length).fill(-1);
-    
-    for(let i = answer.length - 2; i >= 0; i--) {
-        let j = i + 1;
-        
-        while(j < answer.length) {
-            if(numbers[i] < numbers[j]) {
-                biggerNumberIndex[i] = j;
-                answer[i] = numbers[j];
-                break;
-            }
-            
-            if(biggerNumberIndex[j] === -1) {
-                break;
-            }
-            
-            j = biggerNumberIndex[j];
+    const answer = Array(numbers.length).fill(-1);
+    const stack = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        while (stack.length && numbers[stack[stack.length - 1]] < numbers[i]) {
+            const idx = stack.pop();
+            answer[idx] = numbers[i];
         }
+        stack.push(i);
     }
     return answer;
 }
-
-// 다음 인덱스에 대한 행동은 두 가지가 있다. 만약 다음 인덱스의 숫자가 현재보다 작거나 같으면, 그만큼 건너뛸 수 있다.
-// 만약 다음 인덱스의 숫자가 더 크면, biggerNumber 이다.
-// 즉 건너뛰거나 더 큰 숫자이거나 둘 중에 하나.
-
