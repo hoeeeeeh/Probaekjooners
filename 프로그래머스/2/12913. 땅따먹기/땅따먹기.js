42b23a5981061872042f6fe07314309d413a6766
Hoeeeeeh <hoeeeeeh@naver.com>
2025-09-03 16:53:11 +0900

    [level 2] Title: 땅따먹기, Time: 47.00 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,12 @@
+function solution(land) {
+    for (let i = 1; i < land.length; i++) {
+        for (let j = 0; j < 4; j++) {
+            land[i][j] += Math.max(
+                land[i - 1][(j + 1) % 4],
+                land[i - 1][(j + 2) % 4],
+                land[i - 1][(j + 3) % 4]
+            );
+        }
+    }
+    return Math.max(...land[land.length - 1]);
+}
