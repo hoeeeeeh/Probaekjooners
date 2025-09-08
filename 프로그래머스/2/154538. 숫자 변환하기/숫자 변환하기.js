6c87c618c6cb312d399c1a1ff30cb78da15d1c81
Hoeeeeeh <hoeeeeeh@naver.com>
2025-09-08 15:49:18 +0900

    [level 2] Title: 숫자 변환하기, Time: 1186.39 ms, Memory: 290 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,56 +1,22 @@
 function solution(x, y, n) {
-    const q = new Queue();
-    const visited = new Set();
+    const queue = [[x, 0]];
+    let [l, r] = [0, 1];
+    let answer = Infinity;
 
-    q.enqueue([y, 0]);
-    visited.add(y);
-
-    while (!q.isEmpty()) {
-        const [cur, cnt] = q.dequeue();
-
-        if (cur === x) return cnt;
-
-        // -n
-        if (cur - n >= x && !visited.has(cur - n)) {
-            q.enqueue([cur - n, cnt + 1]);
-            visited.add(cur - n);
+    const done = new Set();
+    while (l < r) {
+        const [num, count] = queue[l++];
+        if (done.has(num) || num > y) continue;
+        if (num === y) {
+            answer = Math.min(answer, count);
+            continue;
         }
-        // /2
-        if (cur % 2 === 0 && cur / 2 >= x && !visited.has(cur / 2)) {
-            q.enqueue([cur / 2, cnt + 1]);
-            visited.add(cur / 2);
-        }
-        // /3
-        if (cur % 3 === 0 && cur / 3 >= x && !visited.has(cur / 3)) {
-            q.enqueue([cur / 3, cnt + 1]);
-            visited.add(cur / 3);
-        }
-    }
-    return -1;
-}
+        done.add(num);
 
+        queue[r++] = [num * 3, count + 1];
+        queue[r++] = [num * 2, count + 1];
+        queue[r++] = [num + n, count + 1];
+    }
 
-class Queue {
-    constructor() {
-        this.items = [];
-        this.head = 0;
-        this.tail = 0;
-    }
-    enqueue(item) {
-        this.items[this.tail++] = item;
-    }
-    dequeue() {
-        this.gc();
-        return this.items[this.head++];
-    }
-    isEmpty() {
-        return this.head === this.tail;
-    }
-    gc() {
-        if(this.tail - this.head > 10000) {
-            this.items = this.items.slice(this.head);
-            this.tail -= this.head
-            this.head = 0;
-        }
-    }
+    return answer === Infinity ? -1 : answer;
 }
