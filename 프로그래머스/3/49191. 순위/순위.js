0edc9f0651edef5b4d892386967cc6c015c64cf1
Hoeeeeeh <hoeeeeeh@naver.com>
2025-11-26 19:46:31 +0900

    [level 3] Title: 순위, Time: 12.57 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,35 +1,30 @@
 function solution(n, results) {
-    const graph = Array.from({ length: n}, () => Array(n).fill(null));
-    for(const result of results) {
-        const [winner, loser] = result;
-        graph[winner - 1][loser - 1] = true;
-        graph[loser - 1][winner - 1] = false;
-    }
-    
-    for(let i = 0; i < n; i++) {
-        graph[i][i] = true;
-        dfs(i, i, new Set([i]), true);
-        dfs(i, i, new Set([i]), false);
-    }
-    
-    
-    function dfs(start, cur, visited, win) {
-        for(let next = 0; next < n; next++) {
-            if(visited.has(next)) {
-                continue;
-            }
-            
-            if(graph[cur][next] === null) {
-                continue;
-            }
-            
-            if(graph[cur][next] === win) {
-                visited.add(next);
-                graph[start][next] = win;
-                dfs(start, next, visited, win);
-            }
+  const graph = Array.from({ length: n }, () => Array(n).fill(null));
+
+  for (const [winner, loser] of results) {
+    graph[winner - 1][loser - 1] = true;
+    graph[loser - 1][winner - 1] = false;
+  }
+
+  for (let i = 0; i < n; i++) {
+    graph[i][i] = true;
+  }
+
+  for (let vertex = 0; vertex < n; vertex++) {
+    for (let start = 0; start < n; start++) {
+      if (graph[start][vertex] === null) continue;
+      for (let end = 0; end < n; end++) {
+        if (graph[vertex][end] === null) continue;
+        if (graph[start][end] !== null) continue;
+        if (graph[start][vertex] === graph[vertex][end]) {
+          graph[start][end] = graph[start][vertex];
         }
+      }
     }
-    
-    return graph.filter(m => m.every(w => w !== null)).length;
+  }
+
+return graph.filter((row, i) =>
+  row.filter((v, j) => i !== j && v !== null).length === n - 1
+).length;
+
 }
